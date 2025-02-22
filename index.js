52ee6908b92239048a2601c92de6466f8696cd76
Baby Nap Boltron <147931096+babynapboltron8@users.noreply.github.com>
2025-02-22 10:19:11 +0800

    added model for routes

--- index.js
+++ index.js
@@ -1,9 +1,77 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Product = require('./models/productModel.js');
 const app = express();
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send('Hello from Node API updated');
+});
+
+app.get('/api/products', async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get('/api/products/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post('/api/products', async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//update a product
+app.put('/api/product/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const product = await Product.findByIdAndUpdate(id, req.body);
+
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// delete a product
+
+app.delete('/api/product/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const product = await Product.findByIdAndDelete(id);
+
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found!' });
+    }
+    res.status(200).json({ message: 'Product deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 mongoose
@@ -13,7 +81,7 @@ mongoose
   .then(() => {
     console.log('Connected to the database!');
     app.listen(4000, () => {
-      console.log('Server running on port 3000');
+      console.log('Server running on port 4000');
     });
   })
   .catch(() => {
